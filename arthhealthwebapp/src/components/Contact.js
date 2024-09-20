2ebf2d5a1f3d810a45eb023c8cb205ec26c4cbff
meetpateel <meet.pateel@gmail.com>
2024-09-19 23:44:22 -0500

    update css

--- arthhealthwebapp/src/components/Contact.js
+++ arthhealthwebapp/src/components/Contact.js
@@ -76,88 +76,88 @@ const Contact = () => {
 
     return (
         <div id="contact" className="container">
-        <div className="contact-content">
-            <div className="contact-info">
-                <h2>Contact Arth Physical</h2>
-                <p>
-                    Excited to explore health and recovery? Share some details,
-                    and we will connect with you soon. Eagerly looking forward to
-                    your message!
-                </p>
-                <div className="contact-details">
-                    <p><strong>Phone:</strong> (123) 456-7890</p>
-                    <p><strong>Fax:</strong> (123) 456-7891</p>
-                </div>
-            </div>
-            <form className="contact-form" onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="firstName">Name <span className="required-asterisk">*</span></label>
-                    <div className="name-fields">
-                        <input
-                            type="text"
-                            id="firstName"
-                            name="firstName"
-                            placeholder="First Name"
-                            value={firstName}
-                            onChange={handleChange}
-                            required
-                        />
-                        {errors.firstName && <span className="error">{errors.firstName}</span>}
-                        <input
-                            type="text"
-                            id="lastName"
-                            name="lastName"
-                            placeholder="Last Name"
-                            value={lastName}
-                            onChange={handleChange}
-                            required
-                        />
-                        {errors.lastName && <span className="error">{errors.lastName}</span>}
+            <div className="contact-content">
+                <div className="contact-info">
+                    <h2>Contact Arth Physical</h2>
+                    <p>
+                        Excited to explore health and recovery? Share some details,
+                        and we will connect with you soon. Eagerly looking forward to
+                        your message!
+                    </p>
+                    <div className="contact-details">
+                        <p><strong>Phone:</strong> (123) 456-7890</p>
+                        <p><strong>Fax:</strong> (123) 456-7891</p>
                     </div>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="email">Email <span className="required-asterisk">*</span></label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        placeholder="Email"
-                        value={email}
-                        onChange={handleChange}
-                        required
-                    />
-                    {errors.email && <span className="error">{errors.email}</span>}
-                </div>
-                <div className="form-group">
-                    <label htmlFor="phoneNumber">Phone Number <span className="required-asterisk">*</span></label>
-                    <input
-                        type="tel"
-                        id="phoneNumber"
-                        name="phoneNumber"
-                        placeholder="Phone Number"
-                        value={phoneNumber}
-                        onChange={handleChange}
-                        required
-                    />
-                    {errors.phoneNumber && <span className="error">{errors.phoneNumber}</span>}
-                </div>
-                <div className="form-group">
-                    <label htmlFor="message">Message <span className="required-asterisk">*</span></label>
-                    <textarea
-                        id="message"
-                        name="message"
-                        placeholder="Message"
-                        value={message}
-                        onChange={handleChange}
-                        required
-                    />
-                    {errors.message && <span className="error">{errors.message}</span>}
-                </div>
-                <button type="submit" className="contact-button">Send</button>
                 {isSubmitted && <div className="alert-banner">Sent!</div>}
-            </form>
+                <form className="contact-form" onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="firstName">Name <span className="required-asterisk">*</span></label>
+                        <div className="name-fields">
+                            <input
+                                type="text"
+                                id="firstName"
+                                name="firstName"
+                                placeholder="First Name"
+                                value={firstName}
+                                onChange={handleChange}
+                                required
+                            />
+                            {errors.firstName && <span className="error">{errors.firstName}</span>}
+                            <input
+                                type="text"
+                                id="lastName"
+                                name="lastName"
+                                placeholder="Last Name"
+                                value={lastName}
+                                onChange={handleChange}
+                                required
+                            />
+                            {errors.lastName && <span className="error">{errors.lastName}</span>}
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email <span className="required-asterisk">*</span></label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={handleChange}
+                            required
+                        />
+                        {errors.email && <span className="error">{errors.email}</span>}
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="phoneNumber">Phone Number <span className="required-asterisk">*</span></label>
+                        <input
+                            type="tel"
+                            id="phoneNumber"
+                            name="phoneNumber"
+                            placeholder="Phone Number"
+                            value={phoneNumber}
+                            onChange={handleChange}
+                            required
+                        />
+                        {errors.phoneNumber && <span className="error">{errors.phoneNumber}</span>}
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="message">Message <span className="required-asterisk">*</span></label>
+                        <textarea
+                            id="message"
+                            name="message"
+                            placeholder="Message"
+                            value={message}
+                            onChange={handleChange}
+                            required
+                        />
+                        {errors.message && <span className="error">{errors.message}</span>}
+                    </div>
+                    <button type="submit" className="contact-button">Send</button>
+                </form>
+            </div>
         </div>
-    </div>
     );
 };
 
